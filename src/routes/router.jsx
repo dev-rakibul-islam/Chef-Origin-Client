b9f85c7056b14f627724080140a1dd34ac1f1d35
dev-rakibul-islam <rakibulislam.eb@gmail.com>
2025-12-09 01:23:05 +0600

    first commit

---
+++ src/routes/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>Chef Origin is a Marketplace for Local Home-Cooked Meals</div>
+    ),
+  },
+]);
